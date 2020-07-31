f0000cbe5a40f30e102e7dc8b14043c8333e4d67
Jake Mertz <jakemertz101@gmail.com>
2020-07-31 15:28:15 -0400

    modify HTML and Add PageHeader

--- components/app.js
+++ components/app.js
@@ -6,10 +6,11 @@ class APP {
     this.gradeTable.updateGrades(grades);
     console.log(grades);
   }
-  constructor(gradeTable) {
+  constructor(gradeTable, pageHeader) {
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
     this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
     this.gradeTable = gradeTable;
+    this.pageHeader = pageHeader;
   }
   getGrades() {
     $.ajax({
