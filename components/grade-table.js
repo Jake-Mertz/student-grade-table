e13653949f9e9f6c088a51553b893eb9fec8bebe
Jake Mertz <jakemertz101@gmail.com>
2020-08-11 15:15:32 -0400

    code cleanup

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,20 +6,6 @@ class GradeTable {
   updateGrades(grades) {
     this.tableElement.textContent = '';
 
-    // for (var i = 0; i < grades.length; i++) {
-    //   var newRow = document.createElement('tr');
-    //   var newTd1 = document.createElement('td');
-    //   var newTd2 = document.createElement('td');
-    //   var newTd3 = document.createElement('td');
-    //     newTd1.textContent = grades[i].name;
-    //     newTd2.textContent = grades[i].course;
-    //     newTd3.textContent = grades[i].grade;
-    //   newRow.append(newTd1);
-    //   newRow.append(newTd2);
-    //   newRow.append(newTd3);
-    //   this.tableElement.append(newRow);
-    //   }
-
         for (var z = 0; z < grades.length; z++) {
           this.renderGradeRow(grades[z], this.deleteGrade);
         }
